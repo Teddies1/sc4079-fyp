49b32c17c0f3300adaed976f3dec7ef2344b7d06
Teddies1 <nghonjoo@gmail.com>
2024-07-15 00:16:10 +0800

    added statements to publish sql query results

--- orm.py
+++ orm.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 engine=sqlalchemy.create_engine('sqlite:///../db/azure_packing_trace.db')
 connection = engine.connect()
 
-df = pd.read_sql_query('select * from vmType', connection)
-print(df)
+df1 = pd.read_sql_query('select * from vmType', connection)
+print(df1)
 
+df2 = pd.read_sql_query('select * from vm', connection)
+print(df2)
