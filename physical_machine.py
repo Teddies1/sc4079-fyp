79de0455a0e841417377d5c75069122a60de8a34
Teddies1 <nghonjoo@gmail.com>
2024-07-19 00:43:25 +0800

    created class variables and methods for physical machine class

--- physical_machine.py
+++ physical_machine.py
@@ -1,1 +1,19 @@
-print("test")
+class Machine(object):
+    machine_id: int
+    core_capacity: float
+    memory_capacity: float
+    queue: list
+    
+    def __init__(self, queue: list, machine_id: int=16, core_capacity: float=1, memory_capacity: float=1) -> None:
+        self.machine_id = machine_id
+        self.core_capacity = core_capacity
+        self.memory_capacity = memory_capacity
+        self.queue = queue
+        
+    def add_instance():
+        pass
+    
+    def remove_instance():
+        pass
+    
+    
