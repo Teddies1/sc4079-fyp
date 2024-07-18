037b8cea84a074a01e746b50b1777f3e55c46a08
Teddies1 <nghonjoo@gmail.com>
2024-07-19 00:47:32 +0800

    changed variable name

--- virtual_machine.py
+++ virtual_machine.py
@@ -1,10 +1,10 @@
 class VirtualMachine:
-    vm_id: int
+    vm_type_id: int
     requested_core: float
     requested_memory: float
     
-    def __init__(self, vm_id: int, requested_core: float, requested_memory: float) -> None:
-        self.vm_id = vm_id
+    def __init__(self, vm_type_id: int, requested_core: float, requested_memory: float) -> None:
+        self.vm_type_id = vm_type_id
         self.requested_core = requested_core
         self.requested_memory = requested_memory
         
