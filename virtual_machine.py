017f0988cbb9aa02a7bc7a99681da6a41970a4d6
Teddies1 <nghonjoo@gmail.com>
2024-07-20 01:48:26 +0800

    added variable: list of vm tasks

--- virtual_machine.py
+++ virtual_machine.py
@@ -1,12 +1,18 @@
+from vm_task import Task
+
+
 class VirtualMachine:
     vm_type_id: int
     requested_core: float
     requested_memory: float
+    list_of_tasks: list[Task]
     
-    def __init__(self, vm_type_id: int, requested_core: float, requested_memory: float) -> None:
+    def __init__(self, vm_type_id: int, requested_core: float, requested_memory: float, list_of_tasks: list[Task]) -> None:
         self.vm_type_id = vm_type_id
         self.requested_core = requested_core
         self.requested_memory = requested_memory
+        self.list_of_tasks = list_of_tasks
+        
         
     
         
