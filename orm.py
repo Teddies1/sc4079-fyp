a8d484fb2570f82b71499efa8a33329ceebc9ff2
Teddies1 <nghonjoo@gmail.com>
2024-07-20 01:47:47 +0800

    changed db file path to variable instead of hardcode

--- orm.py
+++ orm.py
@@ -1,11 +1,11 @@
 import sqlalchemy
 import pandas as pd
-from sqlalchemy import create_engine
 
 query = 'select vm.vmId, vm.tenantId, vm.vmTypeId, vm.priority, vm.starttime, vm.endtime, vmType.core, vmType.memory from vm, vmType where vm.vmTypeId = vmType.vmTypeId and endtime <= 14 and machineId = 16;'
-
+path = 'sqlite:///../db/azure_packing_trace.db'
+    
 def get_collated_data(query):    
-    engine=sqlalchemy.create_engine('sqlite:///../db/azure_packing_trace.db')
+    engine=sqlalchemy.create_engine(path)
     connection = engine.connect()
 
     results = pd.read_sql_query(query, connection)
