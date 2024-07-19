d94a88f4584be7307017f51bd60e099fdda38c5e
Teddies1 <nghonjoo@gmail.com>
2024-07-20 01:48:08 +0800

    added bins to store vm instances

--- physical_machine.py
+++ physical_machine.py
@@ -1,17 +1,23 @@
+from virtual_machine import VirtualMachine
+from vm_task import Task
+
 class Machine(object):
     machine_id: int
     core_capacity: float
     memory_capacity: float
     queue: list
+    bins: list[list[VirtualMachine]]
     
-    def __init__(self, queue: list, machine_id: int=16, core_capacity: float=1, memory_capacity: float=1) -> None:
+    def __init__(self, bins: list[list[VirtualMachine]], queue: list, machine_id: int=16, core_capacity: float=1, memory_capacity: float=1) -> None:
         self.machine_id = machine_id
         self.core_capacity = core_capacity
         self.memory_capacity = memory_capacity
         self.queue = queue
+        self.bins = bins
         
-    def add_instance():
+    def add_instance(self, virtual_machine):
         pass
+        
     
     def remove_instance():
         pass
