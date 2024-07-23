4f0e18f2d35fa04036854e66e44ba69e39a1672a
Teddies1 <nghonjoo@gmail.com>
2024-07-23 18:31:15 +0800

    added parameters for dataframe retrieval function

--- orm.py
+++ orm.py
@@ -4,8 +4,8 @@ import pandas as pd
 query = 'select vm.vmId, vm.tenantId, vm.vmTypeId, vm.priority, vm.starttime, vm.endtime, vmType.core, vmType.memory from vm, vmType where vm.vmTypeId = vmType.vmTypeId and endtime <= 14 and machineId = 16;'
 path = 'sqlite:///../db/azure_packing_trace.db'
     
-def get_collated_data(query):    
-    engine=sqlalchemy.create_engine(path)
+def get_collated_data(query, db_path):    
+    engine=sqlalchemy.create_engine(db_path)
     connection = engine.connect()
 
     results = pd.read_sql_query(query, connection)
@@ -13,4 +13,4 @@ def get_collated_data(query):
     connection.close()
     return results
 
-res = get_collated_data(query)
+res = get_collated_data(query, path)
