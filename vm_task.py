88285ba195926dba35b1a925a5b82cb4718f614d
Teddies1 <nghonjoo@gmail.com>
2024-07-23 18:35:29 +0800

    added vm variable to obtain vm type id

--- vm_task.py
+++ vm_task.py
@@ -1,14 +1,17 @@
+from virtual_machine import VirtualMachine
+
 class Task:
+    vm: VirtualMachine
     task_id: int
-    vm_type_id: int
     start_time: float
     end_time: float
     runtime: float
     running: bool = False
     
-    def __init__(self, task_id: int, runtime, float, vm_type_id: int, start_time: float, end_time: float) -> None:
+    def __init__(self, vm: VirtualMachine, task_id: int, runtime: float, start_time: float, end_time: float) -> None:
+        self.vm = vm
         self.task_id = task_id
-        self.vm_type_id = vm_type_id
+        self.vm_type_id = vm.vm
         self.start_time = start_time
         self.end_time = end_time
         self.runtime = runtime
