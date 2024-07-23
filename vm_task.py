4aa5e4f3ef44acb0dbc3ec3f7fb57201560d3a40
Teddies1 <nghonjoo@gmail.com>
2024-07-23 18:32:11 +0800

    removed priority var

--- vm_task.py
+++ vm_task.py
@@ -3,15 +3,15 @@ class Task:
     vm_type_id: int
     start_time: float
     end_time: float
-    priority: int
+    runtime: float
     running: bool = False
     
-    def __init__(self, task_id: int, vm_type_id: int, start_time: float, end_time: float, priority: int) -> None:
+    def __init__(self, task_id: int, runtime, float, vm_type_id: int, start_time: float, end_time: float) -> None:
         self.task_id = task_id
         self.vm_type_id = vm_type_id
         self.start_time = start_time
         self.end_time = end_time
-        self.priority = priority
+        self.runtime = runtime
         
         
     
