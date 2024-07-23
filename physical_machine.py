94e96c19b863267461518e05ffee2fd849e76687
Teddies1 <nghonjoo@gmail.com>
2024-07-23 23:33:37 +0800

    added default value for machines

--- physical_machine.py
+++ physical_machine.py
@@ -1,11 +1,11 @@
 class Machine():
     machine_id: int
-    cpu: float
-    memory: float
+    cpu: float = 1
+    memory: float = 1
     
-    def __init__(self, machine_id: int, cpu: float, memory: float) -> None:
+    def __init__(self, machine_id) -> None:
         self.machine_id = machine_id
-        self.cpu = cpu
-        self.memory = memory
+        self.cpu = 1
+        self.memory = 1
         
     
