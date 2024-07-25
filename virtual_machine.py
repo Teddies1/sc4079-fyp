f9445b5122c575934919f4fccbe540c2b273fdb7
Teddies1 <nghonjoo@gmail.com>
2024-07-26 00:45:26 +0800

    added start time and end time variables

--- virtual_machine.py
+++ virtual_machine.py
@@ -6,11 +6,16 @@ class VirtualMachine:
     requested_core: float
     requested_memory: float
     list_of_tasks: list[Task]
+    starttime: float
+    endtime: float
     runtime: float
-    def __init__(self, vm_type_id: int, requested_core: float, requested_memory: float, runtime: float) -> None:
+    
+    def __init__(self, vm_type_id: int, requested_core: float, requested_memory: float, starttime: float, endtime: float, runtime: float) -> None:
         self.vm_type_id = vm_type_id
         self.requested_core = requested_core
         self.requested_memory = requested_memory
+        self.starttime = starttime
+        self.endtime = endtime
         self.runtime = runtime
         self.list_of_tasks = []
         
