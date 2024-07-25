bca7a549b69bb9bd5c0558a0a55450a627b3760f
Teddies1 <nghonjoo@gmail.com>
2024-07-25 22:30:11 +0800

    changed task list csv to be ordered by start time

--- orm.py
+++ orm.py
@@ -4,7 +4,7 @@ import pandas as pd
 path = 'sqlite:///../db/azure_packing_trace.db'
 
 instance_query = 'select vm.vmTypeId, count(vm.vmId) as taskcount, max((vm.endtime - vm.starttime)) as maxruntime, vmType.core, vmType.memory from vm, vmType where vm.vmTypeId = vmType.vmTypeId and endtime <= 14 and machineId = 16 group by vm.vmTypeId;'
-task_query = 'select vm.vmId as taskId, vm.tenantId, vm.vmTypeId, vm.starttime, vm.endtime, (vm.endtime - vm.starttime) as runtime, vmType.core as requested_core, vmType.memory as requested_memory from vm, vmType where vm.vmTypeId = vmType.vmTypeId and endtime <= 14 and machineId = 16;'
+task_query = 'select vm.vmId as taskId, vm.tenantId, vm.vmTypeId, vm.starttime, vm.endtime, (vm.endtime - vm.starttime) as runtime, vmType.core as requested_core, vmType.memory as requested_memory from vm, vmType where vm.vmTypeId = vmType.vmTypeId and endtime <= 14 and machineId = 16 order by vm.starttime;'
 
 instance_output = "../outputs/vmlist.csv"
 task_output = "../outputs/tasklist.csv"
@@ -19,5 +19,5 @@ def get_collated_data(query, db_path, output_path):
     
     return results
 
-res = get_collated_data(task_query, path)
-vmlist = get_collated_data(instance_query, path, instance_output)
+res = get_collated_data(task_query, path, task_output)
+# vmlist = get_collated_data(instance_query, path, instance_output)
