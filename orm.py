74a57b2ec3b1a143bf6f6805d9e8988f92aa62c7
Teddies1 <nghonjoo@gmail.com>
2024-07-26 00:45:13 +0800

    added query to obtain activated instances according to timestamp

--- orm.py
+++ orm.py
@@ -3,11 +3,13 @@ import pandas as pd
 
 path = 'sqlite:///../db/azure_packing_trace.db'
 
-instance_query = 'select vm.vmTypeId, count(vm.vmId) as taskcount, max((vm.endtime - vm.starttime)) as maxruntime, vmType.core, vmType.memory from vm, vmType where vm.vmTypeId = vmType.vmTypeId and endtime <= 14 and machineId = 16 group by vm.vmTypeId;'
+instance_types_query = 'select vm.vmTypeId, count(vm.vmId) as taskcount, max((vm.endtime - vm.starttime)) as maxruntime, vmType.core, vmType.memory from vm, vmType where vm.vmTypeId = vmType.vmTypeId and endtime <= 14 and machineId = 16 group by vm.vmTypeId;'
 task_query = 'select vm.vmId as taskId, vm.tenantId, vm.vmTypeId, vm.starttime, vm.endtime, (vm.endtime - vm.starttime) as runtime, vmType.core as requested_core, vmType.memory as requested_memory from vm, vmType where vm.vmTypeId = vmType.vmTypeId and endtime <= 14 and machineId = 16 order by vm.starttime;'
+instance_query = 'select vm.vmTypeId, count(vm.vmId) as taskcount, vm.starttime, vm.endtime, max((vm.endtime - vm.starttime)) as maxruntime, vmType.core, vmType.memory from vm, vmType where vm.vmTypeId = vmType.vmTypeId and endtime <= 14 and machineId = 16 group by vm.starttime, vm.vmTypeId;'
 
-instance_output = "../outputs/vmlist.csv"
+instance_types_output = "../outputs/vmlist.csv"
 task_output = "../outputs/tasklist.csv"
+instance_output = "../outputs/assignedinstancelist.csv"
 
 def get_collated_data(query, db_path, output_path):    
     engine=sqlalchemy.create_engine(db_path)
@@ -19,5 +21,6 @@ def get_collated_data(query, db_path, output_path):
     
     return results
 
-res = get_collated_data(task_query, path, task_output)
-# vmlist = get_collated_data(instance_query, path, instance_output)
+# res = get_collated_data(task_query, path, task_output)
+# vm_type_list = get_collated_data(instance_types_query, path, instance_types_output)
+vm_list = get_collated_data(instance_query, path, instance_output)
