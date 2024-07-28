0612ad20fabbab9ce6552fad69fc8f32e63aca60
Teddies1 <nghonjoo@gmail.com>
2024-07-28 21:39:21 +0800

    added print debug statements and tested packer functionality

--- scheduler.py
+++ scheduler.py
@@ -35,20 +35,12 @@ class Scheduler():
         self.vm_types = pd.read_csv("../outputs/vmlist2.csv")
         
     def load_tasks_to_bins(self, list_of_tasks):
-        # f = open("../outputs/tasklist2.csv", 'r')
-        # reader = csv.reader(f)
-        # next(reader)
-        # print(self.task_bins)
         for row in list_of_tasks:
             task = Task(int(row[0]), int(row[2]), float(row[5]), float(row[3]), float(row[4]))
             index = self.obtain_bin_index(self.task_bins, task.runtime)
             self.task_bins[index].append(task)
             
     def load_vms_to_bins(self, list_of_vms):
-        # f = open("../outputs/assignedinstancelist2.csv", 'r')
-        # reader = csv.reader(f)
-        # next(reader)
-         
         for row in list_of_vms:
             instance = VirtualMachine(int(row[0]), float(row[5]), float(row[6]), float(row[2]), float(row[3]), float(row[4]))
             index = self.obtain_bin_index(self.instance_bins, instance.runtime)
@@ -56,6 +48,7 @@ class Scheduler():
             
     def obtain_bin_index(self, bins, runtime):
         # [0, 1), [1, 2), [2, 4), [4, 8), [8, 16)
+        runtime = float(runtime)
         if runtime < 1:
             return 0
         
@@ -65,38 +58,45 @@ class Scheduler():
         return bin_index
         
     def packer(self, list_of_tasks: list[Task], list_of_vms, task_bins: list[list[Task]], instance_bins:list[list[VirtualMachine]]):
-        list_of_tasks.sort(key=lambda x: x.runtime, reverse=True)
-        max_runtime_index = self.obtain_bin_index(task_bins, list_of_tasks[0].runtime)
+        list_of_tasks.sort(key=lambda x: float(x[5]), reverse=True)
+        max_runtime_index = self.obtain_bin_index(task_bins, list_of_tasks[0][5])
         count = 0
         #sort the unscheduled tasks based on the runtime descending
         self.load_tasks_to_bins(list_of_tasks)
         self.load_vms_to_bins(list_of_vms)
+        
         #iterate unscheduled tasks
         for task in task_bins[max_runtime_index]:
+            print("iterate inside bin for task with runtime:", task.runtime)
             #eligible instances  check if any instance with same bin is eligible to take the task
             count = 0
             for instance in instance_bins[max_runtime_index]:
-                if instance.requested_core <= self.core_capacity and instance.requested_memory <= self.memory_capacity:
-                    count += 1
+                if instance.runtime == task.runtime or math.isclose(instance.runtime, task.runtime):
+                    if instance.requested_core <= self.core_capacity and instance.requested_memory <= self.memory_capacity:
+                        count += 1
             #if eligible instances not empty
+            print("eligible instances:", count)
             if count > 0:
                 #assign the task to the instance with remaining runtime closest to the task with the same vmTypeId
                 task_runtime = task.runtime
                 for instance in instance_bins[max_runtime_index]:
+                    print(instance.runtime)
                     if instance.runtime == task_runtime or math.isclose(instance.runtime, task_runtime):
                         if instance.requested_core <= self.core_capacity and instance.requested_memory <= self.memory_capacity:
+                            print("sucessfully assigned task to instance")
                             task.assigned = True
                             instance.list_of_tasks.append(task)
                             #add task’s requested CPU and memory to memory capacity
                             self.core_capacity -= instance.requested_core
                             self.memory_capacity -= instance.requested_memory
+                            print("current core and memory capacity is: ", self.core_capacity, self.memory_capacity)
                             break
             #else
             else:
-                uppack_index = max_runtime_index
+                uppack_index = max_runtime_index + 1
                 #uppack_eligible_instances  check if any instance with greater bins is eligible
                 while uppack_index < len(task_bins):
-                    uppack_index += 1
+                    print("uppacking starts")
                     count = 0
                     for instance in instance_bins[uppack_index]:
                         if instance.requested_core <= self.core_capacity and instance.requested_memory <= self.memory_capacity:
@@ -107,17 +107,20 @@ class Scheduler():
                         resource_sorted_instance_list = sorted(instance_bins[uppack_index], key=lambda x: (x.requested_core, x.requested_memory), reverse=True)
                         for instance in resource_sorted_instance_list:
                             if instance.requested_core <= self.core_capacity and instance.requested_memory <= self.memory_capacity:
+                                print("uppacking assignment successful")
                                 task.assigned = True
                                 instance.list_of_tasks.append(task)
                                 #add task’s requested CPU and memory to memory capacity
                                 self.core_capacity -= instance.requested_core
                                 self.memory_capacity -= instance.requested_memory
                                 break
+                    uppack_index += 1
                 #else
                 if task.assigned == False:
                     #downppack_elgible_instances  check if any lower bins instance is eligible'
                     downpack_index = max_runtime_index
                     while downpack_index >= 0:
+                        print("downpacking starts")
                         downpack_index -= 1
                         count = 0
                         for instance in instance_bins[downpack_index]:
@@ -130,6 +133,7 @@ class Scheduler():
                             for index, instance in enumerate(resource_sorted_instance_list):
                                 if instance.requested_core <= self.core_capacity and instance.requested_memory <= self.memory_capacity:
                                 #assign to the instance with most available resources
+                                    print("downpacking assignment successful")
                                     task.assigned = True
                                     instance.list_of_tasks.append(task)
                                     #add task’s requested CPU and memory to memory capacity
@@ -140,7 +144,7 @@ class Scheduler():
                             promoted_instance = instance_bins[downpack_index].pop(promoted_index)
                             instance_bins[max_runtime_index].append(promoted_instance)
                         
-        
+            
     def scaling(self, task_bins, instance_bins):
         '''
         Input Task Bins, Instance Bins, Unscheduled Tasks
@@ -182,11 +186,32 @@ def main():
     scheduled in batches during a periodic scheduling event; the frequency
     of the scheduling event is configurable.
     '''
-    
+    fourteen_days = 1209600
     machine = Machine(16)
     sched = Scheduler(machine)
-    sched.load_tasks_to_bins()
-    sched.load_vms_to_bins()
+    
+    taskfile = open("../outputs/tasklist2.csv", 'r')
+    taskreader = csv.reader(taskfile)
+    next(taskreader)
+    
+    instancefile = open("../outputs/assignedinstancelist2.csv", 'r')
+    instancereader = csv.reader(instancefile)
+    next(instancereader)
+    
+    tasklist = []
+    instancelist = []
+    
+    for row in taskreader:
+        if float(row[3]) <= 1:
+            tasklist.append(row)
+    for row in instancereader:
+        if float(row[2]) <= 1:
+            instancelist.append(row)
+
+    sched.packer(list_of_tasks=tasklist, list_of_vms=instancelist, task_bins=sched.task_bins, instance_bins=sched.instance_bins)    
+    
+    
+    
     
     
 if __name__ == "__main__":
