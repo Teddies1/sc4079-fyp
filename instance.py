deeb9e4404fea603284a3d7c3263e694fcbf08b9
Teddies1 <nghonjoo@gmail.com>
2024-09-15 01:40:40 +0800

    import task

--- instance.py
+++ instance.py
@@ -1,11 +1,25 @@
+from vm_task import Task
+
 class Instance():
     machine_id: int
-    cpu: float = 1
-    memory: float = 1
+    core_capacity: float = 1
+    memory_capacity: float = 1
+    list_of_tasks: list[Task]
+    starttime: float
+    endtime: float
+    max_runtime: float = 0
     
-    def __init__(self, machine_id) -> None:
+    def __init__(self, machine_id: int) -> None:
         self.machine_id = machine_id
-        self.cpu = 1
-        self.memory = 1
+        self.list_of_tasks = []
+    
+    def get_max_runtime(self) -> float:
+        if len(self.list_of_tasks) > 0:
+            self.max_runtime = max(self.list_of_tasks, key=lambda x: x.runtime)
+        
+        return self.max_runtime
+    
+    
+        
         
     
