102117f991e6a0af5f7aae89f1188546ea1a166e
Teddies1 <nghonjoo@gmail.com>
2024-10-02 23:35:16 +0800

    added print debug to fix scaler memory leak

--- scheduler.py
+++ scheduler.py
@@ -56,6 +56,9 @@ class Scheduler():
         return bin_index
     
     def load_tasks_to_bins(self, list_of_tasks, algo) -> None:
+        print("loading tasks to bins")
+        self.task_bins = [[] for _ in range(self.no_of_bins)]
+        
         for row in list_of_tasks:
             task = Task(int(row['taskId']), int(row['vmTypeId']), float(row['runtime']) ,float(row['starttime']), float(row['endtime']), float(row['requested_core']), float(row['requested_memory']))
             if algo == "stratus":
@@ -65,6 +68,7 @@ class Scheduler():
             self.task_bins[index].append(task)
             
     def update_instance_bins(self, algo):
+        print("updating instance bins")
         self.instance_bins = [[] for _ in range(self.no_of_bins)]
         
         for instance in self.instance_pool:
@@ -78,6 +82,8 @@ class Scheduler():
                     self.instance_bins[runtime_bin_index].append(instance)
                     
     def free_expired_tasks_and_instances_stratus(self, timestamp) -> None:
+        print("free stratus")
+        
         for instance in self.instance_pool:
             instance.list_of_tasks.sort(key=lambda x: x.end_time)
             for task in instance.list_of_tasks:
@@ -89,20 +95,22 @@ class Scheduler():
             
         self.instance_pool[:] = [instance for instance in self.instance_pool if len(instance.list_of_tasks) > 0]
 
-        for bin in self.task_bins:
-            bin[:] = [task for task in bin if task.end_time > timestamp]
-            bin[:] = [task for task in bin if task.assigned == False]
+        # for bin in self.task_bins:
+        #     bin[:] = [task for task in bin if task.end_time > timestamp]
+        #     bin[:] = [task for task in bin if task.assigned == False]
             
-    def free_expired_tasks_and_instances_baseline(self, timestamp) -> None:    
+    def free_expired_tasks_and_instances_baseline(self, timestamp) -> None:
+        print("free baseline") 
         for instance in self.instance_pool:
             instance.list_of_tasks[:] = [task for task in instance.list_of_tasks if task.end_time > timestamp]
         
         self.instance_pool[:] = [instance for instance in self.instance_pool if len(instance.list_of_tasks) > 0]
         
-        self.task_bins[0][:] = [task for task in self.task_bins[0] if task.end_time > timestamp]
-        self.task_bins[0][:] = [task for task in self.task_bins[0] if task.assigned == False]
+        # self.task_bins[0][:] = [task for task in self.task_bins[0] if task.end_time > timestamp]
+        # self.task_bins[0][:] = [task for task in self.task_bins[0] if task.assigned == False]
         
     def packer(self, list_of_tasks) -> None:
+        print("packer")
         count = 0
         #sort the unscheduled tasks based on the runtime descending
         self.load_tasks_to_bins(list_of_tasks, "stratus")      
@@ -194,6 +202,7 @@ class Scheduler():
                             downpack_index -= 1
                             
     def scaling(self) -> None:
+        print("scaler")
         candidate_group_list: list[list[Task]] = []
         unassigned_task_list: list[Task] = []
         # get max instance runtime, cpu and memory capacity
@@ -202,6 +211,7 @@ class Scheduler():
         min_runtime_instance = min(self.instance_pool, key=lambda x: x.max_runtime)
         scaler_min_runtime = min_runtime_instance.max_runtime
         # get min instance cpu and memory capacity
+        
         min_cpu_instance = min(self.instance_pool, key=lambda x: x.core_capacity)
         scaler_min_cpu = min_cpu_instance.core_capacity
         min_memory_instance = min(self.instance_pool, key=lambda x: x.memory_capacity)
@@ -210,7 +220,10 @@ class Scheduler():
         max_cpu_instance = max(self.instance_pool, key=lambda x: x.core_capacity)
         scaler_max_cpu = max_cpu_instance.core_capacity
         max_memory_instance = max(self.instance_pool, key=lambda x: x.memory_capacity)
-        scaler_max_memory = max_memory_instance.memory_capacity        
+        scaler_max_memory = max_memory_instance.memory_capacity
+                
+        print(scaler_max_cpu, scaler_max_memory, scaler_max_runtime)
+        print(scaler_min_memory, scaler_min_cpu, scaler_min_runtime)
         
         for i in range(len(self.task_bins)-1, -1, -1):
             for task in self.task_bins[i]:
@@ -220,28 +233,34 @@ class Scheduler():
         unassigned_task_list.sort(key=lambda x: x.runtime, reverse=True)
         candidate_group_flag = 0
         candidate_group_size = 1
+        print("unassigned tasks", len(unassigned_task_list))
         # for each candidate group i do 
         if len(unassigned_task_list) > 0:
-            while candidate_group_flag == 0: 
+            while candidate_group_flag == 0:
+                if unassigned_task_list_pointer >= len(unassigned_task_list):
+                    break 
                 unassigned_task_list_pointer = 0
                 cumulative_task_memory = 0
                 cumulative_task_cpu = 0
                 new_candidate_group = []
                 # add tasks to candidate groups until number of tasks = i 
                 while unassigned_task_list_pointer < len(unassigned_task_list) and len(new_candidate_group) < candidate_group_size:
+                    print("pointer: ", unassigned_task_list_pointer)
                     new_unassigned_task = unassigned_task_list[unassigned_task_list_pointer]
                     unassigned_task_list_pointer += 1
                     
                     new_candidate_group.append(new_unassigned_task)
                     cumulative_task_memory += task.requested_memory
                     cumulative_task_cpu += task.requested_core
+                    print("cumulative group cpu: ", cumulative_task_cpu)
+                    print("cumulative group memory: ", cumulative_task_memory)
                 # or task group cumulative memory > instance memory and cpu > instance cpu
                 if cumulative_task_cpu <= 1 and cumulative_task_memory <= 1:
                     candidate_group_list.append([new_candidate_group, cumulative_task_cpu, cumulative_task_memory])
                     candidate_group_size += 1
                 else:
                     candidate_group_flag = 1
-                    
+        print("number of candidate groups: ", len(candidate_group_list))
         # score = normalised used constraining resource / cost    
         # for each candidate group, it calculates the Score for each instance
         max_efficiency_score = -1
@@ -250,21 +269,32 @@ class Scheduler():
         max_cumulative_memory = 0
         # for each candidate group do
         for group in candidate_group_list:
+            print("group")
             # for each instance in instance pool do
             for instance in self.instance_pool:
+                print("instance")
                 score = 0
                 candidate_instance_group = (group[0], instance.machine_id)
                 cpu_ratio = group[1]
                 memory_ratio = group[2]
                 constraining_resource = min(cpu_ratio, memory_ratio)
                 if cpu_ratio < memory_ratio:
-                    normalised_constraining_resouce = (constraining_resource - scaler_min_cpu) / (scaler_max_cpu - scaler_min_cpu)
+                    if (scaler_max_cpu - scaler_min_cpu) > 0:
+                        normalised_constraining_resouce = (constraining_resource - scaler_min_cpu) / (scaler_max_cpu - scaler_min_cpu)
+                    else:
+                        normalised_constraining_resouce = 0
                 else:
-                    normalised_constraining_resouce = (constraining_resource - scaler_min_memory) / (scaler_max_memory - scaler_min_memory)
+                    if (scaler_max_memory - scaler_min_memory) > 0:
+                        normalised_constraining_resouce = (constraining_resource - scaler_min_memory) / (scaler_max_memory - scaler_min_memory)
+                    else: 
+                        normalised_constraining_resouce = 0
+                print("normalised constraining resource: ", normalised_constraining_resouce)
                 # note: since cost is proportional to runtime, we use normalised runtime for our cost
                 normalised_runtime = (instance.max_runtime - scaler_min_runtime)/ (scaler_max_runtime - scaler_min_runtime)
+                print("normalised runtime: ", normalised_runtime)
                 if normalised_runtime > 0:
                     score = normalised_constraining_resouce / normalised_runtime
+                    print("score", score)
                 else:
                     score = 0
                 if score > max_efficiency_score:
@@ -286,16 +316,17 @@ class Scheduler():
             self.instance_pool.append(new_max_instance)
             
     def stratus(self, total_time, interval) -> None:
+        print("stratus")
         self.unique_id_pointer = 35
         
         task_csv = pd.read_csv("../outputs/tasklist2.csv")        
         task_csv_pointer = 0
         
-        self.average_stratus_core_log = []
-        self.average_stratus_memory_log = []
-        self.average_max_runtime_stratus_log = []
-        self.percentage_assigned_tasks_stratus_log = []
-        self.number_of_instances_stratus_log = []
+        # self.average_stratus_core_log = []
+        # self.average_stratus_memory_log = []
+        # self.average_max_runtime_stratus_log = []
+        # self.percentage_assigned_tasks_stratus_log = []
+        # self.number_of_instances_stratus_log = []
         self.stratus_logging_list = []
         
         for i in range(1, total_time, interval):
@@ -320,6 +351,7 @@ class Scheduler():
             # self.average_max_runtime_stratus_log.append(runtime_sum / instance_pool_length)
             # self.number_of_instances_stratus_log.append(instance_pool_length)
             # self.percentage_assigned_tasks_stratus_log.append(unassigned_tasks / total_tasks)
+            
             self.stratus_logging_list.append({
                 "timestamp": i,
                 "avg_stratus_runtime": runtime_sum / instance_pool_length,
@@ -329,6 +361,7 @@ class Scheduler():
             })
             
     def baseline_algo(self, list_of_tasks) -> None:
+        print("baseline algorithm")
         self.load_tasks_to_bins(list_of_tasks, "baseline")
         
         for task in self.task_bins[0]:
@@ -343,6 +376,7 @@ class Scheduler():
                 self.instance_pool.append(new_instance)     
                 
     def baseline(self, total_time, interval) -> None:
+        print("baseline")
         self.unique_id_pointer = 0
         
         self.instance_pool = []
@@ -352,11 +386,11 @@ class Scheduler():
         task_csv = pd.read_csv("../outputs/tasklist2.csv")        
         task_csv_pointer = 0
         
-        self.average_baseline_core_log = []
-        self.average_baseline_memory_log = []
-        self.average_max_runtime_baseline_log = []
-        self.percentage_assigned_tasks_baseline_log = []
-        self.number_of_instances_baseline_log = []
+        # self.average_baseline_core_log = []
+        # self.average_baseline_memory_log = []
+        # self.average_max_runtime_baseline_log = []
+        # self.percentage_assigned_tasks_baseline_log = []
+        # self.number_of_instances_baseline_log = []
         self.baseline_logging_list = []
         
         for i in range(1, total_time, interval):
@@ -397,18 +431,18 @@ def main() -> None:
     
     print("-----Running Stratus Algo-----")
     tic1 = time.perf_counter()
-    sched.stratus(test_duration, interval)
+    sched.stratus(fourteen_days, interval)
     toc1 = time.perf_counter()  
     print("-----Finished Stratus Algo-----")
     print("-----Running Baseline Algo-----")
     tic2 = time.perf_counter()
-    sched.baseline(test_duration, interval)
+    sched.baseline(fourteen_days, interval)
     toc2 = time.perf_counter()
     print("-----Finished Baseline Algo-----")
     print(f"-----Finished Stratus Algo in: {toc1 - tic1:0.4f} seconds -----")
     print(f"-----Finished Baseline Algo in: {toc2 - tic2:0.4f} seconds -----")
     
-    timestamp_array = [i for i in range(1, test_duration, interval)]
+    timestamp_array = [i for i in range(1, fourteen_days, interval)]
     
     print("-----Writing to CSV-----")
     # with open(f"../logging/core_usage.csv", "w") as f:
